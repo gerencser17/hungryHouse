631a0cfc41c579bb5cd19653aebb048e6d89a90f
gerencser17 <gerencser17rgmail.com>
2020-06-16 13:39:31 +0200

    First commit

---
+++ precache-manifest.832faadcbcc189b5875b66194ccc1e5e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5149b767cbb6ae86ece097ec676940f0",
+    "url": "/hungryHouse/index.html"
+  },
+  {
+    "revision": "54d8574b8c7f0cb63646",
+    "url": "/hungryHouse/static/css/main.2e268ce3.chunk.css"
+  },
+  {
+    "revision": "d47482fbc7c0d75bdff5",
+    "url": "/hungryHouse/static/js/2.b6f744da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hungryHouse/static/js/2.b6f744da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54d8574b8c7f0cb63646",
+    "url": "/hungryHouse/static/js/main.1e2d4c8a.chunk.js"
+  },
+  {
+    "revision": "e717a6a25619b1a0ea4c",
+    "url": "/hungryHouse/static/js/runtime-main.75a105e9.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/hungryHouse/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/hungryHouse/static/media/background_search_mobile.5d0905e6.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hungryHouse/static/media/logo.5d5d9eef.svg"
+  }
+]);
